4de5753941bd363f074d1abbb0336e854481982e
keep-promise <64321089+keep-promise@users.noreply.github.com>
2023-05-24 19:52:53 +0800

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  entry: './app.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react']
+          }
+        }
+      },
+    ]
+  }
+};
